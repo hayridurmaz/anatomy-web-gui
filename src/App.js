ce4498a2667c89c3fe9624b0b9a4b9cb8b0604a6
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-15 01:39:47 +0300

    server connection ready!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    response: ""
+  };
+  request = () => {
+    var http = new XMLHttpRequest();
+    var url = "https://anatomy-web.herokuapp.com/";
+    var params = "";
+    http.open("GET", url, true);
+    http.setRequestHeader("Content-type", "text/plain");
+
+    http.onreadystatechange = () => {
+      // console.log(http.readyState);
+      console.log(http.status);
+
+      if (http.readyState === 4 && http.status === 200) {
+        // alert(http.responseText);
+        this.setState({ response: http.responseText });
+      }
+    };
+    http.send(params);
+  };
+
+  componentDidMount() {
+    this.request();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>{this.state.response}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
