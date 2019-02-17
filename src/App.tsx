64d6ca4545b88f59e44aaa6c86016027fcc8ba91
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-17 11:17:48 +0300

    http requrdt added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 class App extends React.Component {
+  state = {
+    response: "GELMEDİ"
+  };
+  request = () => {
+    var http = new XMLHttpRequest();
+    var url = "http://188.166.49.57:8080/";
+    var params = "";
+    http.open("GET", url, true);
+    http.setRequestHeader("Content-type", "text/plain");
+
+    http.onreadystatechange = () => {
+      // console.log(http.readyState);
+      console.log(http.status);
+
+      if (http.readyState === 4 && http.status === 200) {
+        // alert(http.responseText);
+        this.setState({ response: http.responseText });
+      }
+    };
+    http.send(params);
+  };
+
+  componentDidMount() {
+    this.request();
+  }
   public render() {
     return (
       <div className="App">
@@ -12,7 +37,7 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          GELDİ Mİ: <code>{this.state.response}</code>
         </p>
       </div>
     );
