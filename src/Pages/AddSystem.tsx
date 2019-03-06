10d6e041f6fad0453a383db974353849ca84f35a
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-03-06 22:58:43 +0300

    Adding system added

--- src/Pages/AddSystem.tsx
+++ src/Pages/AddSystem.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import ToggleButton from "react-toggle-button";
 import { getDate } from "../utils/utils";
 import Axios from "axios";
-import { Segment } from "semantic-ui-react";
+import { Segment, Button, Form, Message } from "semantic-ui-react";
 import { format } from "path";
 var ProgressBar = require("react-progressbar").default;
 
@@ -16,219 +16,66 @@ interface ReduxProps {
 }
 class AddSystem extends React.Component<IProps & ReduxProps> {
   state = {
-    news: "",
     title: "",
-    tarih: "",
-    yazar: "Öğrenci Konseyi",
-    systemsArray: [] as types.System[],
-    selectedDuyuruTipi: "duyuru",
-    imgUrl: "",
-    isUploading: false,
-    progress: 0
+    systemsArray: [] as types.System[]
   };
 
   handleSubmit = event => {
-    if (this.state.news === "" || this.state.title === "") {
-      alert("Lütfen boş alan bırakmayın");
+    if (this.state.title === "") {
+      alert("Please do not leave title empty");
       event.preventDefault();
 
       return;
     }
-    if (window.confirm("Haberi göndermek istediğine emin misin?")) {
-      /* let news = {
-        author: this.state.yazar,
-        content: this.state.news,
-        date: this.state.tarih,
-        header: this.state.title,
-        type: this.state.selectedDuyuruTipi,
-        links: [{ url: "" }],
-        id: "",
-        valid: true,
-        image: this.state.imgUrl
-      }; */
+    if (window.confirm("Are you sure you want to add the system?")) {
       //TODO: Send to the webservice
-      this.setState({ news: "", title: "" });
+      Axios.post("http://188.166.49.57:8080/Systems", {
+        name: this.state.title
+      }).then(response => {
+        console.log(response.data);
+        this.getSystems();
+      });
+      this.setState({ title: "" });
     }
     event.preventDefault();
   };
-  componentWillMount() {
-    this.setState({ tarih: getDate() });
-    //TODO: Get
-  }
 
-  componentDidMount() {
+  getSystems = () => {
     Axios.get("http://188.166.49.57:8080/Systems").then(response => {
       this.setState({ systemsArray: response.data });
       console.log(response.data);
     });
+  };
+  componentDidMount() {
+    this.getSystems();
   }
 
   renderSystems = () => {
-    if (this.state.systemsArray.length <= 10) {
-      return this.state.systemsArray.map((item, id) => {
-        return <Segment pilled>{item.name}</Segment>;
-      });
-    } else {
-      let arr = this.state.systemsArray.slice(
-        this.state.systemsArray.length - 10,
-        this.state.systemsArray.length
+    return this.state.systemsArray.map((item, id) => {
+      return (
+        <Segment key={id} pilled="true">
+          {item.name}
+        </Segment>
       );
-      return arr.map((item, id) => {
-        return (
-          <div key={id}>
-            <Link to={{ pathname: "/detailNews", state: { news: item } }}>
-              <h3 className={"white-text"}>{item["header"]}</h3>
-            </Link>
-            <p key={id}>
-              {"" + item["content"].substr(0, 30)}
-              ...
-            </p>
-            Gösterilsin mi:{" "}
-            <ToggleButton
-              value={item["valid"] || false}
-              onToggle={value => {
-                item["valid"] = !value;
-              }}
-            />
-            <hr />
-          </div>
-        );
-      });
-    }
-  };
-  handleOptionChange = changeEvent => {
-    this.setState({
-      selectedDuyuruTipi: changeEvent.target.value
     });
   };
 
-  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
-  handleProgress = progress => this.setState({ progress });
-  handleUploadError = error => {
-    this.setState({ isUploading: false });
-    console.error(error);
-  };
-  handleUploadSuccess = filename => {
-    this.setState({ avatar: filename, progress: 100, isUploading: false });
-  };
-
   form = () => {
-    let progressing;
-    if (this.state.isUploading || this.state.progress === 100) {
-      progressing = (
-        <div
-          style={{
-            width: 150,
-            borderStyle: "solid",
-            borderColor: "green"
-          }}
-        >
-          <ProgressBar completed={this.state.progress} />
-        </div>
-      );
-    } else {
-      progressing = <div />;
-    }
     return (
       <form onSubmit={this.handleSubmit}>
         <div style={{ flex: 1, margin: 10 }}>
           <label>
-            Başlık:
+            System name:
             <input
               value={this.state.title}
-              style={{ marginLeft: 20 }}
+              style={{ marginLeft: 20, marginRight: 20 }}
               onChange={e => {
                 this.setState({ title: e.target.value });
               }}
             />
+            <input type="submit" value="Submit" />
           </label>
         </div>
-
-        <label>
-          <div style={{ flex: 1, margin: 10 }}>
-            <p> Haber:</p>
-            <textarea
-              value={this.state.news}
-              style={{ height: 300, width: 900 }}
-              onChange={e => {
-                this.setState({ news: e.target.value });
-              }}
-            />
-          </div>
-        </label>
-        <div style={{ flex: 1, margin: 10 }}>
-          <label>
-            Tarih:
-            <input value={this.state.tarih} style={{ marginLeft: 20 }} />
-          </label>
-        </div>
-        <div style={{ flex: 1, margin: 10 }}>
-          <label>
-            Yazar:
-            <input
-              value={this.state.yazar}
-              contentEditable={true}
-              style={{ marginLeft: 20 }}
-            />
-          </label>
-        </div>
-
-        <div style={{ flex: 1, margin: 10 }}>
-          <label>
-            Resim:
-            {/* <FileUploader
-            accept="image/*"
-            name="avatar"
-            randomizeFilename
-            storageRef={firebase.storage().ref("images")}
-            onUploadStart={this.handleUploadStart}
-            onUploadError={this.handleUploadError}
-            onUploadSuccess={this.handleUploadSuccess}
-            onProgress={this.handleProgress}
-          /> */}
-            {progressing}
-          </label>
-        </div>
-
-        <div style={{ flex: 1, margin: 10 }}>
-          <label>
-            Haber tipi:
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="duyuru"
-                  checked={this.state.selectedDuyuruTipi === "duyuru"}
-                  onChange={this.handleOptionChange}
-                />
-                Duyuru
-              </label>
-            </div>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="etkinlik"
-                  checked={this.state.selectedDuyuruTipi === "etkinlik"}
-                  onChange={this.handleOptionChange}
-                />
-                Etkinlik
-              </label>
-            </div>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="haber"
-                  checked={this.state.selectedDuyuruTipi === "haber"}
-                  onChange={this.handleOptionChange}
-                />
-                Haber
-              </label>
-            </div>
-          </label>
-        </div>
-        <input type="submit" value="Submit" />
       </form>
     );
   };
@@ -236,11 +83,11 @@ class AddSystem extends React.Component<IProps & ReduxProps> {
   render() {
     if (this.props.isLoggedIn) {
       return (
-        <div className={"container"}>
-          <h1 className={"teal-text"}> Haber ekle</h1>
-          <div className={"card-pannel z-depth-5 teal"}>{this.form()}</div>
-          <h1 className={"teal-text"}> Haberler</h1>
+        <div className={"container"} style={{ flexDirection: "row", flex: 1 }}>
+          <h1 className={"teal-text"}> Systems </h1>
           <Segment.Group>{this.renderSystems()}</Segment.Group>
+          <h1 className={"teal-text"}> Add new system </h1>
+          <div className={"card-pannel z-depth-5 teal"}>{this.form()}</div>
         </div>
       );
     } else {
