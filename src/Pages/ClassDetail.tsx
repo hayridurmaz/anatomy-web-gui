701e9de4e3fe24e58bb2c58b2bfcf06be1981c82
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-03-22 01:54:57 +0300

    material-ui and dialog added

--- src/Pages/ClassDetail.tsx
+++ src/Pages/ClassDetail.tsx
@@ -4,6 +4,12 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router";
 import { List, Icon, Button } from "semantic-ui-react";
 import AddQuiz from "./AddQuiz";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { TextField } from "@material-ui/core";
 
 interface Iprops {
   news: any;
@@ -14,6 +20,44 @@ interface ReduxProps {
 }
 class ClassDetail extends React.Component<Iprops & ReduxProps> {
 
+  state = {
+    dialogAddStudent: false,
+    dialogAddTeacher: false,
+    dialogAddQuiz: false,
+    teacherUsername: "",
+    studentUsername: "",
+    quizName: ""
+
+  };
+
+  handleClickOpenTeacher = () => {
+    this.setState({ dialogAddTeacher: true });
+  };
+
+  handleCloseTeacher = () => {
+    this.setState({ dialogAddTeacher: false });
+  };
+
+  handleClickOpenQuiz = () => {
+    this.setState({ dialogAddQuiz: true });
+  };
+
+  handleCloseQuiz = () => {
+    this.setState({ dialogAddQuiz: false });
+  };
+
+  handleClickOpenStudent = () => {
+    this.setState({ dialogAddStudent: true });
+  };
+
+  handleCloseStudent = () => {
+    this.setState({ dialogAddStudent: false });
+  };
+  handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
   renderQuizzes = () => {
     let class_obj = this.props.location.state.classes as types.Class;
@@ -75,18 +119,107 @@ class ClassDetail extends React.Component<Iprops & ReduxProps> {
       return (
         <div className={"container "}>
           <h1>{class_obj.name}</h1>
-          <h2>Quizzes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button basic className="ui button" onClick={(event) => { }} color='green' id="but" style={{ margin: 0, marginLeft: 2, borderRadius: 2, fontWeight: "bold", fontSize: 11.5, width: 43 }} > <Icon name="add circle" ></Icon></Button></h2>
+          <h2>Quizzes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button basic className="ui button" onClick={(event) => { this.handleClickOpenQuiz() }} color='green' id="but" style={{ margin: 0, marginLeft: 2, borderRadius: 2, fontWeight: "bold", fontSize: 11.5, width: 43 }} > <Icon name="add circle" ></Icon></Button></h2>
           <List>
             {this.renderQuizzes()}
           </List>
-          <h2>Teachers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button basic className="ui button" onClick={(event) => { }} color='green' id="but" style={{ margin: 0, marginLeft: 2, borderRadius: 2, fontWeight: "bold", fontSize: 11.5, width: 43 }} > <Icon name="add user" ></Icon></Button></h2>
+          <Dialog
+            open={this.state.dialogAddQuiz}
+            onClose={this.handleCloseQuiz}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Add quiz"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Please enter a quiz name.
+            </DialogContentText>
+              <TextField
+                onChange={this.handleChange('quizName')}
+                id="standard-with-placeholder"
+                label="Quiz name"
+                placeholder=""
+                className={"textField"}
+                margin="normal"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleCloseQuiz} color="red">
+                Cancel
+            </Button>
+              <Button onClick={this.handleCloseQuiz} color="green" autoFocus>
+                Add
+            </Button>
+            </DialogActions>
+          </Dialog>
+
+          <h2>Teachers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button basic className="ui button" onClick={(event) => { this.handleClickOpenTeacher() }} color='green' id="but" style={{ margin: 0, marginLeft: 2, borderRadius: 2, fontWeight: "bold", fontSize: 11.5, width: 43 }} > <Icon name="add user" ></Icon></Button></h2>
           <List>
             {this.renderTeachers()}
           </List>
-          <h2>Students&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button basic className="ui button" onClick={(event) => { }} color='green' id="but" style={{ margin: 0, marginLeft: 2, borderRadius: 2, fontWeight: "bold", fontSize: 11.5, width: 43 }} > <Icon name="add user" ></Icon></Button></h2>
+          <Dialog
+            open={this.state.dialogAddTeacher}
+            onClose={this.handleCloseTeacher}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Add teacher"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Please enter a teacher username to add.
+            </DialogContentText>
+              <TextField
+                onChange={this.handleChange('teacherUsername')}
+                id="standard-with-placeholder"
+                label="Username"
+                placeholder=""
+                className={"textField"}
+                margin="normal"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleCloseTeacher} color="red">
+                Cancel
+            </Button>
+              <Button onClick={this.handleCloseTeacher} color="green" autoFocus>
+                Add
+            </Button>
+            </DialogActions>
+          </Dialog>
+
+          <h2>Students&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button basic className="ui button" onClick={(event) => { this.handleClickOpenStudent() }} color='green' id="but" style={{ margin: 0, marginLeft: 2, borderRadius: 2, fontWeight: "bold", fontSize: 11.5, width: 43 }} > <Icon name="add user" ></Icon></Button></h2>
           <List>
             {this.renderStudents()}
           </List>
+          <Dialog
+            open={this.state.dialogAddStudent}
+            onClose={this.handleCloseStudent}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Add student"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Please enter a student username to add.
+            </DialogContentText>
+              <TextField
+                onChange={this.handleChange('studentUsername')}
+                id="standard-with-placeholder"
+                label="Username"
+                placeholder=""
+                className={"textField"}
+                margin="normal"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleCloseStudent} color="red">
+                Cancel
+            </Button>
+              <Button onClick={this.handleCloseStudent} color="green" autoFocus>
+                Add
+            </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       );
     } else {
